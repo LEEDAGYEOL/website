3cbc6d5ef7e228d13f4f2a4f1c26d125a899e96b
LEEDAGYEOL <anslvhehekd@gmail.com>
2022-05-16 14:22:42 +0900

    miyadakkyori

---
+++ js/swiper.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".basic2" ,{
+    
+       
+    breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
+});
